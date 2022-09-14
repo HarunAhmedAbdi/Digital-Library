add43dbad31db0dfd8d16cb212686339e384cd91
Harun <harunahmedabdi@gmail.com>
2022-09-14 16:31:45 +0100

    created book entity

---
+++ src/main/java/com/project/books/Books.java
@@ -0,0 +1,71 @@
+package com.project.books;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Books {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+
+    // Default constructor
+    public Books() {
+        super();
+    }
+
+    public Books(String accountNumber, String password, String firstName, String lastName, LocalDate dob) {
+        super();
+        this.accountNumber = accountNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getaccountNumber() {
+        return accountNumber;
+    }
+
+    public void setaccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
